c4f6a2132aefca0c37088e032d42c5391d4acaff
Andreas Weizel <rpinski@awzhome.de>
2020-08-13 00:39:25 +0200

    Some visual improvements for plan cards and execution progress

---
+++ PPBackup.WinApp/ViewModel/BoolToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PPBackup.WinApp.ViewModel
+{
+    class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as bool?).GetValueOrDefault() ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as Visibility?).GetValueOrDefault() == Visibility.Visible;
+        }
+    }
+}
