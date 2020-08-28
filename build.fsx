ca6dfda76815167dd48025a1e518c013a12b7dbe
Andreas Weizel <rpinski@awzhome.de>
2020-08-29 01:44:10 +0200

    Add InnoSetup definition file for installer compilation Extend build script to output artifacts and build Win installer

--- build.fsx
+++ build.fsx
@@ -1,26 +1,43 @@
 #r "paket:
 nuget Fake.IO.FileSystem
 nuget Fake.DotNet.Cli //
-nuget Fake.Core.Target //"
+nuget Fake.Core.Target //
+nuget Fake.Installer.InnoSetup //"
 #load "./.fake/build.fsx/intellisense.fsx"
 
 open Fake.Core
 open Fake.DotNet
 open Fake.Core.TargetOperators
+open Fake.Core.Operators
+open Fake.IO.FileSystemOperators
+open Fake.Installer
 
 // *** Define Targets ***
 Target.create "Clean" (fun _ ->
-  DotNet.exec (fun defaults -> defaults) "clean" "PPBackup.sln" |> ignore
+  DotNet.exec (fun p -> p) "clean" "PPBackup.sln" |> ignore
 )
 
 Target.create "Build" (fun _ ->
-  DotNet.build (fun defaults -> defaults) "PPBackup.sln"
+  DotNet.build (fun p -> p) "PPBackup.sln"
 )
 
-Target.create "CleanBuild" (fun _ ->
-  Target.run 1 "Clean" []
-  Target.run 1 "Build" []
+Target.create "Publish" (fun _ ->
+  DotNet.build (fun p ->
+  { p with
+      OutputPath = Some("build" @@ "publish")
+  }) ("PPBackup.WinApp" @@ "PPBackup.WinApp.csproj")
 )
+
+Target.create "Installer-Win" (fun _ ->
+  InnoSetup.build (fun p -> 
+  { p with 
+     ScriptFile = "ppbackup.iss"
+     ToolPath = Environment.ProgramFilesX86 @@ "Inno Setup 6" @@ "ISCC.exe"
+  })
+)
+
+"Publish"
+  ==> "Installer-Win"
 
 // *** Start Build ***
-Target.runOrDefault "Build"
+Target.runOrDefaultWithArguments "Build"
---
+++ ppbackup.iss
@@ -0,0 +1,40 @@
+#define APP_NAME "PPBackup"
+#define APP_VERSION "0.1"
+#define APP_PUBLISHER "Andreas Weizel"
+#define APP_URL "https://github.com/awzhome/pp-backup"
+#define APP_EXE_FILE "ppbackup-ui.exe"
+
+[Setup]
+AppId={{D32AB7B1-334D-43A1-9A7C-EE692FCA7A8A}
+AppName={#APP_NAME}
+AppVersion={#APP_VERSION}
+AppVerName={#APP_NAME} {#APP_VERSION}
+AppPublisher={#APP_PUBLISHER}
+AppPublisherURL={#APP_URL}
+AppSupportURL={#APP_URL}
+AppUpdatesURL={#APP_URL}
+DefaultDirName={localappdata}\{#APP_NAME}
+DisableProgramGroupPage=yes
+PrivilegesRequired=lowest
+OutputBaseFilename=ppbackup-setup-win64-{#APP_VERSION}
+OutputDir=build\installer
+Compression=lzma
+SolidCompression=yes
+WizardStyle=modern
+
+[Languages]
+Name: "english"; MessagesFile: "compiler:Default.isl"
+
+[Tasks]
+Name: "desktopicon"; Description: "{cm:CreateDesktopIcon}"; GroupDescription: "{cm:AdditionalIcons}"; Flags: unchecked
+
+[Files]
+Source: "build\publish\*"; DestDir: "{app}"; Flags: ignoreversion
+
+[Icons]
+Name: "{autoprograms}\{#APP_NAME}"; Filename: "{app}\{#APP_EXE_FILE}"
+Name: "{autodesktop}\{#APP_NAME}"; Filename: "{app}\{#APP_EXE_FILE}"; Tasks: desktopicon
+
+[Run]
+Filename: "{app}\{#APP_EXE_FILE}"; Description: "{cm:LaunchProgram,{#StringChange(APP_NAME, '&', '&&')}}"; Flags: nowait postinstall skipifsilent
+
