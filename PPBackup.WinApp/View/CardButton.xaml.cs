a96f3c82db0e4c051345cae5cbf973d45d8d1d8f
Andreas Weizel <rpinski@awzhome.de>
2020-09-02 22:49:58 +0200

    Implement buttons on plan cards as separate UserControl (CardButton)

---
+++ PPBackup.WinApp/View/CardButton.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PPBackup.WinApp.View
+{
+    /// <summary>
+    /// Interaction logic for CardButton.xaml
+    /// </summary>
+    public partial class CardButton : UserControl
+    {
+        public ICommand Command
+        {
+            get => (ICommand) GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand),
+                typeof(CardButton), new PropertyMetadata(null));
+
+        public string Text
+        {
+            get => (string) GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string),
+                typeof(CardButton), new PropertyMetadata(null));
+
+        public CardButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
