7f902516d19b102fcd2f590761f21c6ceb57464d
Andreas Weizel <rpinski@awzhome.de>
2021-03-19 00:11:30 +0100

    Add Avalonia-based project skeleton from template

---
+++ PPBackup.Gui/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PPBackup.Gui.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Perspectives Pictured Backup";
+    }
+}
