4c1b312cdd3a0726ca1686b8c08e65700bf2cc89
Andreas Weizel <rpinski@awzhome.de>
2021-04-06 17:44:18 +0200

    Add buttons to move and delete step entries in plan editor

--- PPBackup.Gui/ViewModels/EditBackupPlanViewModel.cs
+++ PPBackup.Gui/ViewModels/EditBackupPlanViewModel.cs
@@ -1,16 +1,27 @@
 ﻿using ReactiveUI;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive;
 
 namespace PPBackup.Gui.ViewModels
 {
     public class EditBackupPlanViewModel : ViewModelBase
     {
         public ReactiveCommand<bool, bool> CloseCommand { get; }
+        public ReactiveCommand<Unit, Unit> RemoveStepCommand { get; }
+        public ReactiveCommand<Unit, Unit> MoveStepUpCommand { get; }
+        public ReactiveCommand<Unit, Unit> MoveStepDownCommand { get; }
 
         public EditBackupPlanViewModel(BackupPlanViewModel backupPlanViewModel)
         {
             CloseCommand = ReactiveCommand.Create<bool, bool>(save => save);
+
+            RemoveStepCommand = ReactiveCommand.Create(RemoveStep,
+                this.WhenAnyValue(x => x.SelectedItem, (EditStepViewModelBase? selectedItem) => selectedItem != null));
+            MoveStepUpCommand = ReactiveCommand.Create(MoveStepUp,
+                this.WhenAnyValue(x => x.SelectedItem, selectedItem => Steps.FirstOrDefault() != selectedItem));
+            MoveStepDownCommand = ReactiveCommand.Create(MoveStepDown,
+                this.WhenAnyValue(x => x.SelectedItem, selectedItem => Steps.LastOrDefault() != selectedItem));
 
             InitializeFromBasicModel(backupPlanViewModel);
             SelectedItem = Steps.FirstOrDefault();
@@ -29,7 +40,11 @@ namespace PPBackup.Gui.ViewModels
         public EditStepViewModelBase? SelectedItem
         {
             get => selectedItem;
-            set => this.RaiseAndSetIfChanged(ref selectedItem, value);
+            set
+            {
+                selectedItem = value;
+                this.RaisePropertyChanged();
+            }
         }
 
         private void InitializeFromBasicModel(BackupPlanViewModel baseModel)
@@ -39,6 +54,55 @@ namespace PPBackup.Gui.ViewModels
             foreach (var step in baseModel.Steps)
             {
                 Steps.Add(step.ToViewModel());
+            }
+        }
+
+        private void RemoveStep()
+        {
+            if (SelectedItem != null)
+            {
+                int selectedIndex = Steps.IndexOf(SelectedItem);
+                Steps.Remove(SelectedItem);
+                if (Steps.Count > 0)
+                {
+                    if (Steps.Count <= selectedIndex)
+                    {
+                        selectedIndex = Steps.Count - 1;
+                    }
+                    SelectedItem = Steps[selectedIndex];
+                }
+                else
+                {
+                    SelectedItem = null;
+                }
+            }
+        }
+
+        private void MoveStepUp()
+        {
+            if (SelectedItem != null)
+            {
+                int selectedIndex = Steps.IndexOf(SelectedItem);
+                if (selectedIndex > 0)
+                {
+                    Steps.Move(selectedIndex, selectedIndex - 1);
+                    SelectedItem = null;
+                    SelectedItem = Steps[selectedIndex - 1];
+                }
+            }
+        }
+
+        private void MoveStepDown()
+        {
+            if (SelectedItem != null)
+            {
+                int selectedIndex = Steps.IndexOf(SelectedItem);
+                if (selectedIndex < Steps.Count - 1)
+                {
+                    Steps.Move(selectedIndex, selectedIndex + 1);
+                    SelectedItem = null;
+                    SelectedItem = Steps[selectedIndex + 1];
+                }
             }
         }
     }
