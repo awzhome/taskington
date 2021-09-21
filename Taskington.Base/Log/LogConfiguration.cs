ae976db96f03806417d2ba190bda7ecfd319eac9
Andreas Weizel <rpinski@awzhome.de>
2021-09-22 00:57:41 +0200

    Fix issue setting minimum log level from configuration

--- Taskington.Base/Log/LogConfiguration.cs
+++ Taskington.Base/Log/LogConfiguration.cs
@@ -1,22 +1,40 @@
 using Taskington.Base.Config;
 using Taskington.Base.Events;
+using Taskington.Base.Service;
 
 namespace Taskington.Base.Log
 {
-    class LogConfiguration
+    class LogConfiguration : IAutoInitializable
     {
+        private readonly IApplicationEvents applicationEvents;
+        private readonly ILog log;
+        private readonly ConfigurationManager configurationManager;
+
         public LogConfiguration(IApplicationEvents applicationEvents, ILog log, ConfigurationManager configurationManager)
         {
+            this.applicationEvents = applicationEvents;
+            this.log = log;
+            this.configurationManager = configurationManager;
+        }
+
+        public void Initialize()
+        {
+            UpdateMinimumLevel();
             applicationEvents.ConfigurationReloaded += (o, e) =>
             {
-                (log as IReconfigurableLog)?.SetMiminumLevel(configurationManager.GetValue("log") switch
-                {
-                    "verbose" => LogLevel.Verbose,
-                    "warning" => LogLevel.Warning,
-                    "error" => LogLevel.Error,
-                    _ => LogLevel.Info,
-                });
+                UpdateMinimumLevel();
             };
+        }
+
+        private void UpdateMinimumLevel()
+        {
+            (log as IReconfigurableLog)?.SetMiminumLevel(configurationManager.GetValue("log") switch
+            {
+                "verbose" => LogLevel.Verbose,
+                "warning" => LogLevel.Warning,
+                "error" => LogLevel.Error,
+                _ => LogLevel.Info,
+            });
         }
     }
 }
