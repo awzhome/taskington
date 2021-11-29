3e630d4d1f74d8fe34aa471615a69fb8d929907c
Andreas Weizel <rpinski@awzhome.de>
2021-11-30 00:43:53 +0100

    Customize MSI dialogs

--- Taskington.Installer.Windows/setup.cs
+++ Taskington.Installer.Windows/setup.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 
 using WixSharp;
+using WixSharp.Controls;
 
 namespace Taskington.Installer.Windows
 {
@@ -15,11 +16,12 @@ namespace Taskington.Installer.Windows
             var buildOutputDir = $@"output\publish-win64";
 
             var project = new Project("Taskington",
-                              new InstallDir(@"%LocalAppData%\Programs\Taskington\bin",
+                              new InstallDir(@"%LocalAppData%\Programs\Taskington",
                                   new DirFiles(Path.Combine(buildOutputDir, "*.*"))));
 
             project.GUID = new Guid("91c41e60-c478-4ce8-a299-568bd3258b20");
             project.Version = new Version(AppPackage.NumericVersion);
+            project.LicenceFile = Path.Combine(Environment.CurrentDirectory, "license.rtf");
             project.SourceBaseDir = Path.GetDirectoryName(Environment.CurrentDirectory);
             project.InstallScope = InstallScope.perUser;
             project.InstallPrivileges = InstallPrivileges.limited;
@@ -31,7 +33,7 @@ namespace Taskington.Installer.Windows
                 DowngradeErrorMessage = "A newer release of Taskington is already installed on this system. Please uninstall it first to continue."
             };
 
-            project.UI = WUI.WixUI_ProgressOnly;
+            project.UI = WUI.WixUI_Minimal;
 
             project.ResolveWildCards().FindFile(f => f.Name.EndsWith("taskington-gui.exe")).First()
                 .Shortcuts = new[] {
