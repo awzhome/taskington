14b1d5fa3caaeff161c6e43ee24c7ebf2d9d30d2
Andreas Weizel <rpinski@awzhome.de>
2022-09-13 01:25:19 +0200

    Allow to create parameterless RequestMessages

--- Taskington.Base/TinyBus/RequestMessage.cs
+++ Taskington.Base/TinyBus/RequestMessage.cs
@@ -5,6 +5,23 @@ using Taskington.Base.TinyBus.Endpoints;
 
 namespace Taskington.Base.TinyBus;
 
+public abstract record RequestMessage<R>
+{
+    private static readonly RequestMessageEndPoint<R> messageEndPoint = new();
+
+    public IEnumerable<R> Request() =>
+        messageEndPoint.Request();
+
+    public static void Subscribe(Func<R> subscriber) =>
+        messageEndPoint.Subscribe(subscriber);
+
+    protected static void Unsubscribe(Func<R> subscriber) =>
+        messageEndPoint.Unsubscribe(subscriber);
+
+    protected static void UnsubscribeAll() =>
+        messageEndPoint.UnsubscribeAll();
+}
+
 public abstract record RequestMessage<T, R> where T : RequestMessage<T, R>
 {
     private static readonly RequestMessageEndPoint<T, R> messageEndPoint = new();
@@ -27,6 +44,9 @@ public abstract record RequestMessage<T, R> where T : RequestMessage<T, R>
 
 public static class RequestMessageExtensions
 {
+    public static IEnumerable<R> RequestMany<R>(this RequestMessage<IEnumerable<R>> message) =>
+       message.Request().SelectMany(r => r);
+
     public static IEnumerable<R> RequestMany<T, R>(this RequestMessage<T, IEnumerable<R>> message)
          where T : RequestMessage<T, IEnumerable<R>> =>
        message.Request().SelectMany(r => r);
