24aa14f2af68b2a70b3d25d505642a9add7f8ad3
Andreas Weizel <rpinski@awzhome.de>
2026-06-11 00:06:42 +0200

    Remove TinyBus from project

--- Taskington.Base/TinyBus/SubscriberInfo.cs
+++
@@ -1,7 +0,0 @@
-namespace Taskington.Base.TinyBus;
-
-public class SubscriberInfo<F, P>(F subscriber, P? predicate = default)
-{
-    public F Subscriber { get; } = subscriber;
-    public P? Predicate { get; } = predicate;
-}
